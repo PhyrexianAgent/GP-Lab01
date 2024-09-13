a7ac4de3ce72c9c3261406114e70f07fab616cac
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-09-13 14:06:11 -0400

    Instantiates All Points On Start
    Also set positions for each of the 8 points.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Vector3[] points = new Vector3[8];
+    [SerializeField] private GameObject pointPrefab;
+
+    private Rigidbody rigidbody;
+    private List<GameObject> pointObjects = new List<GameObject>();
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        MakePoints();
+    }
+
+    void MakePoints(){
+        foreach (Vector3 point in points){
+            pointObjects.Add(Instantiate(pointPrefab, point, Quaternion.identity));
+        }
     }
 
     // Update is called once per frame
